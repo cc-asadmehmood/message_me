93dce1ac73802e05873705ac46015092d1955e3c
Asad Mehmood <asadmehmood7748@gmail.com>
2025-04-23 10:30:17 +0500

    message me demo app on ruby on rails.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Add Semantic UI assets to the asset load path
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "semantic-ui-sass")
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
